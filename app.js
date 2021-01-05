eb001930e20d0e689626c2779ee6bbd856f71e62
Vipul Chauhan <viratdev07@gmail.com>
2021-01-05 09:26:15 +0530

    Added Description

--- app.js
+++ app.js
@@ -5,27 +5,54 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 
-let express = require("express");
-let app = express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/public'));
-
 let ejs = require("ejs");
 let pdf = require("html-pdf");
 let path = require("path");
+let express = require("express");
+let app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+
+
+
 const { urlencoded } = require('express');
 
+let desc_list = [];
+let price_list = [];
+let company_name = "";
+let address = "";
+let pan = "";
+let gst = "";
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/field.html");
+    res.render('field');
 });
 
-app.post("/", function(req,res) {
-    console.log(res)
-})
 
-app.get("/generateReport", (req, res) => {
-    ejs.renderFile(path.join(__dirname, './views/', "report-template.ejs"), {students: students}, (err, data) => {
+app.post("/", function (req, res) {
+    company_name = req.body.company;
+    address = req.body.address;
+    pan = req.body.pan;
+    gst = req.body.gst;
+    console.log(req.body);
+    res.redirect("/desc");
+});
+
+app.get("/desc", function (req, res) {
+    res.render("description", {desc: desc_list, price: price_list});
+});
+
+app.post("/desc", function(req,res) {
+    var description = req.body.desc;
+    var price = req.body.price;
+        desc_list.push(description);
+        price_list.push(price);
+    res.redirect("/desc");
+});
+
+app.get("click", (req, res) => {
+    ejs.renderFile(path.join(__dirname, './views/', "report-template.ejs"), {students: desc}, (err, data) => {
     if (err) {
           res.send(err);
     } else {
